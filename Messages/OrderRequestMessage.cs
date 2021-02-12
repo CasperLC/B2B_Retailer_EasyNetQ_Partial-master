48f142c3c9280ee7cf3e3aab1d3d37a2a8599a00
CasperLC <43999061+CasperLC@users.noreply.github.com>
2021-02-12 11:29:44 +0100

    Initial commit

---
+++ Messages/OrderRequestMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace Messages
+{
+    public class OrderRequestMessage
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return "OrderId" + this.OrderId +", CustomerId" + this.CustomerId + ", ProductId" + this.ProductId + ", Country" + this.Country;
+        }
+    }
+}
